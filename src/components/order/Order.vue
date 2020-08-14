7bcc2a72dc12119b1be1d06f37db27babb3f0947
highfish <854639450@qq.com>
2020-08-14 22:06:04 +0800

    vue_shop_commit

---
+++ src/components/order/Order.vue
@@ -0,0 +1,200 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 搜索与添加区域 -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getOrderList">
+            <el-button slot="append" icon="el-icon-search" @clear="getOrderList"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
+      <!-- 订单列表区域 -->
+      <el-table :data="orderlist" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price" width="95px"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status" width="70px">
+          <template slot-scope="scope">
+            <el-tag size="mini" type="danger" v-if="scope.row.pay_status==='0'">未付款</el-tag>
+            <el-tag type="success" size="mini" v-else>付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send" width="70px"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time" width="140px">
+          <template slot-scope="scope">{{scope.row.create_time | dateFormat}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template slot-scope="scope">
+            <!-- 修改按钮 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+              @click="showEditDialog(scope.row.order_id)"
+            ></el-button>
+            <!-- 物流进度 -->
+            <el-button
+              type="success"
+              icon="el-icon-location"
+              size="mini"
+              @click="showProgoresssBox"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
+
+    <!-- 修改的对话框 -->
+    <el-dialog
+      title="修改订单地址"
+      :visible.sync="editDialogVisible"
+      width="50%"
+      @close="editDialogClosed"
+    >
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
+        <el-form-item label="省/县/乡" prop="address1">
+          <el-cascader :options="catyDate" v-model="editForm.address1"></el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="editForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 物流进度的对话框 -->
+    <el-dialog title="物流进度" :visible.sync="progressVisible" width="50%">
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in professform"
+          :key="index"
+          :timestamp="activity.time"
+        >{{activity.context}}</el-timeline-item>
+      </el-timeline>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import catyDate from './citydata.js'
+export default {
+  data() {
+    return {
+      // 获取订单列表的参数对象
+      queryInfo: {
+        query: '',
+        // 当前的页数
+        pagenum: 1,
+        // 当前每页显示多少条数据
+        pagesize: 10,
+      },
+      total: 0,
+      orderlist: [],
+      // 控制修改用户对话框的显示与隐藏
+      editDialogVisible: false,
+      progressVisible: false,
+      editForm: {
+        address2: '',
+        address1: [],
+      },
+      // 修改表单的验证规则对象
+      editFormRules: {
+        address1: [
+          { required: true, message: '请选择省市区', trigger: 'blur' },
+        ],
+        address2: [
+          { required: true, message: '请填写详细地址', trigger: 'blur' },
+        ],
+      },
+      catyDate,
+      // 物流信息
+      professform: [],
+    }
+  },
+  created() {
+    // 获取当前订单列表
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单列表失败！')
+      }
+      this.orderlist = res.data.goods
+      this.total = res.data.total
+      console.log(res)
+    },
+    // 监听 pagesize 改变的事件
+    handleSizeChange(newSize) {
+      // console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    // 监听 页码值 改变的事件
+    handleCurrentChange(newPage) {
+      console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    },
+    // 展示编辑用户的对话框
+    async showEditDialog(id) {
+      // console.log(id)
+      const { data: res } = await this.$http.get('orders/' + id)
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('查询失败！')
+      }
+
+      this.editForm = res.data
+      this.editDialogVisible = true
+    },
+    // 监听修改用户对话框的关闭事件
+    editDialogClosed() {
+      this.$refs.editFormRef.resetFields()
+    },
+    // 物流进度
+    async showProgoresssBox() {
+      const { data: res } = await this.$http.get('/kuaidi/1106975712662')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取物流失败！')
+      }
+      this.professform = res.data
+      console.log(this.professform)
+      this.progressVisible = true
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.el-cascader {
+  width: 100%;
+}
+</style>
